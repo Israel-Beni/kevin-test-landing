338701109d49461fbd03d94015c55bf77e91cfc8
Israel-Beni <bendegbe72@gmail.com>
2026-02-10 16:00:54 +0100

    feat: Introduce new landing sections for team and branding, supported by new UI components, graphics, and shadcn/ui configuration.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,7 @@
+
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { figtree } from "./fonts";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -25,9 +16,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${figtree.className} antialiased`}
       >
-        {children}
+        <main className="flex flex-col gap-5 md:gap-10 lg:gap-20 py-12 md:py-16 lg:py-23">
+          {children}
+        </main>
       </body>
     </html>
   );
