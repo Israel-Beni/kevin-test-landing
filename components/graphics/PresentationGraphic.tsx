8b77bc25acdbc7dac604517397efe3f76ad3a63c
Israel-Beni <bendegbe72@gmail.com>
2026-02-11 02:29:03 +0100

    refactor: Adjust presentation graphic visibility for smaller screens and optimize teams SVG attributes.

--- components/graphics/PresentationGraphic.tsx
+++ components/graphics/PresentationGraphic.tsx
@@ -16,7 +16,7 @@ export function PresentationGraphic({ className }: PresentationGraphicProps) {
   const [isHovering, setIsHovering] = useState(false)
   return (
     <div className="w-full h-59 max-w-96 relative">
-      <ConnectingLineGraphic className="absolute top-1/2 w-[46%] max-[1200px]:" />
+      <ConnectingLineGraphic className="absolute top-1/2 w-[46%] max-[1200px]:hidden" />
       <div className="absolute w-81/92 h-full top-0 right-0 max-w-81 ">
         <div className="relative size-full">
           <AIApplicationGraphic className={cn("absolute aspect-253/142 w-253/324 transition-all duration-100 ease-in-out scale-100 hover:scale-140  hover:shadow-[0px_12px_16px_-4px_rgba(16,24,40,0.08)]", currentHoverPosition === 0 && "z-300")} onMouseEnter={() => { setCurrentHoverPosition(0); setIsHovering(true); }} onMouseLeave={() => { setCurrentHoverPosition(-1); setIsHovering(false) }} />
