22626b0ef60109d331cb549c31a30890ce3c2253
Israel-Beni <bendegbe72@gmail.com>
2026-02-11 01:39:44 +0100

    feat: Add `@svgr/webpack` for SVG component imports and update the `teams.svg` graphic.

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // experimental: {
+  //   turbopackMemoryLimit: 4096,
+  // },
+  turbopack: {
+    rules: {
+      '*.svg': {
+        loaders: ['@svgr/webpack'],
+        as: '*.js',
+      },
+    },
+  },
 };
 
 export default nextConfig;
